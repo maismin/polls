bc85b10c1297c2143100d00e8c7557ecdb851aa4
Simon Mai <maismin@gmail.com>
2019-10-15 16:13:11 -0400

    Add logger middleware

--- app/middleware/index.js
+++ app/middleware/index.js
@@ -1,6 +1,7 @@
 import thunk from 'redux-thunk';
 import { applyMiddleware } from 'redux';
+import logger from './logger';
 
-const middleware = [thunk];
+const middleware = [thunk, logger];
 
 export default applyMiddleware(...middleware);
