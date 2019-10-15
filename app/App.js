be2cc91add46097dd87247ad199b11dc845c5e5a
Simon Mai <maismin@gmail.com>
2019-10-15 18:17:47 -0400

    Create Dashboard component

--- app/App.js
+++ app/App.js
@@ -3,15 +3,25 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { handleInitialData } from './actions/shared';
 
+import Dashboard from './components/Dashboard';
+
 function App(props) {
+  const { loading } = props;
   useEffect(() => {
     props.handleInitialData();
   }, []);
-  return <div>Hello World</div>;
+  return <div>{loading ? null : <Dashboard />}</div>;
 }
 
 App.propTypes = {
+  loading: PropTypes.bool.isRequired,
   handleInitialData: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = ({ authedUser }) => {
+  return {
+    loading: authedUser === null,
+  };
 };
 
 const mapDispatchToProps = {
@@ -19,6 +29,6 @@ const mapDispatchToProps = {
 };
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps,
 )(App);
---
+++ app/components/Dashboard.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+function Dashboard(props) {
+  const [showAnswered, setShowAnswered] = useState(false);
+
+  const { answered, unanswered } = props;
+  const list = showAnswered ? answered : unanswered;
+
+  return (
+    <div>
+      <div className="dashboard-toggle">
+        <button
+          type="button"
+          style={{ textDecoration: !showAnswered ? 'underline' : null }}
+          onClick={() => setShowAnswered(false)}
+        >
+          Unanswered
+        </button>
+        <span>|</span>
+        <button
+          type="button"
+          style={{ textDecoration: showAnswered ? 'underline' : null }}
+          onClick={() => setShowAnswered(true)}
+        >
+          Answered
+        </button>
+      </div>
+      <ul className="dashboard-list">
+        {list.map(poll => (
+          <li key={poll.id}>{poll.question}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+Dashboard.propTypes = {
+  answered: PropTypes.arrayOf(PropTypes.object).isRequired,
+  unanswered: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = ({ authedUser, polls, users }) => {
+  const answers = users[authedUser] ? users[authedUser].answers : [];
+
+  const answered = answers
+    .map(id => polls[id])
+    .sort((a, b) => b.timestamp - a.timestamp);
+
+  const unanswered = Object.keys(polls)
+    .filter(id => !answers.includes(id))
+    .map(id => polls[id])
+    .sort((a, b) => b.timestamp - a.timestamp);
+
+  return {
+    answered,
+    unanswered,
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
